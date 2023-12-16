08a13361f5d8f6e4c2ac5812573be7f6e4f05ad5
mehraankush <mehraankush36@gmail.com>
2023-12-16 16:46:19 +0530

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+import AuthorLayout from "@/components/about/AuthorLayout";
+
+export default function Page() {
+
+  return (
+    <>
+      <AuthorLayout>
+        <p className="mb-5">
+            Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds
+        </p>
+        <p className="mb-5">
+            Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds
+        </p>
+        <p>
+            Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds
+        </p>
+      </AuthorLayout>
+    </>
+  )
+}
